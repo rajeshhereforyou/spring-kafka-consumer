6ab6a9f69a6067af5880f4e8b1f445680330e10a
Rajesh Gedela <rajesh.gedela@philips.com>
2020-04-06 15:21:56 -0400

    Added files

---
+++ src/main/java/com/example/kafka/consumerretry/ConsumerRetryApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka.consumerretry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerRetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerRetryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafka/consumerretry/ConsumerRetryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.kafka.consumerretry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsumerRetryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
