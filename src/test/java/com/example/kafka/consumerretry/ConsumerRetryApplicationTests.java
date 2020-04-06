4775d4b24a687660e6c108810b2f6d9b8319ff53
Rajesh Gedela <rajesh.gedela@philips.com>
2020-04-06 15:28:07 -0400

    adding files

--- src/test/java/com/example/kafka/consumerretry/ConsumerRetryApplicationTests.java
+++ src/test/java/com/example/kafka/consumerretry/ConsumerRetryApplicationTests.java
@@ -1,16 +1,28 @@
 package com.example.kafka.consumerretry;
 
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ConsumerRetryApplicationTests {
 
+	@ClassRule
+	public static EmbeddedKafkaRule embedded = new EmbeddedKafkaRule(1);
+
+	@Autowired
+	private KafkaTemplate<String, String> template;
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws InterruptedException {
+		this.template.send("so60641945", "bar");
+		Thread.sleep(30_000);
 	}
 
 }
